07e40163530f7f7451fbd35287dc9989d6982cc2
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2022-01-25 11:20:27 -0600

    Update sketch.js

--- Graphics/Graphics Assignment 1/Example 4/sketch.js
+++ Graphics/Graphics Assignment 1/Example 4/sketch.js
@@ -1,1 +1,32 @@
+/* 
+Annabelle Kanchirathingal
+Drawing Circle + Star
+*/
+function setup()
+{
+	createCanvas(200, 200);
+}
 
+function draw()
+{
+	background(0, 0, 155);
+	strokeWeight(3);
+	stroke(255, 255, 255);
+	fill(0, 130, 0);
+	ellipse(100, 100, 100, 100);
+
+	fill(255, 0, 0);
+	beginShape();
+	vertex(100, 48);
+	vertex(85, 80);
+	vertex(50, 80);
+	vertex(80, 105);
+	vertex(70, 137);
+	vertex(100, 120);
+	vertex(136, 140);
+	vertex(120, 110);
+	vertex(155, 80);
+	vertex(115, 80);
+	vertex(100, 48);
+	endShape(CLOSE);
+}
