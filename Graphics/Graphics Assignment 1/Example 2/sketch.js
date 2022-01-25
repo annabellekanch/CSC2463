3b0ab8f3476d909a2eae0d3e82c6eb98620b2672
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2022-01-25 10:32:57 -0600

    Update sketch.js

--- Graphics/Graphics Assignment 1/Example 2/sketch.js
+++ Graphics/Graphics Assignment 1/Example 2/sketch.js
@@ -1,1 +1,50 @@
+/*
+Annabelle Kanchirathingal
+Drawing of primary colors
+*/
 
+function setup() 
+{
+	createCanvas(200, 200);
+}
+
+function draw() 
+{	
+	background(255, 255, 255);
+	
+	//same width, height, and transparency for all ellipses
+	var width = 90;
+	var height = 90;
+	var transparency = 90;
+	noStroke();
+
+	//RED
+	var x = 90;
+	var y = 60;
+	var red = 255;
+	var green = 0;
+	var blue = 0;
+	fill(red, green, blue, transparency)
+	ellipse(x, y, width, height);
+
+	//GREEN
+	x = 120;
+	y = 110;
+	var red2 = 0;
+	var green2 = 255;
+	var blue2 = 0;
+	fill(red2, green2, blue2, transparency)
+	ellipse(x, y, width, height);
+		
+	//BLUE
+	x = 60;
+	y = 110;
+	var red3= 0;
+	var green3 = 0;
+	var blue3 = 255;
+	fill(red3, green3, blue3, transparency)
+	ellipse(x, y, width, height);
+	
+
+	
+}
