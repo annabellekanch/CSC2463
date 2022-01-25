649335c74f28c3b395c219f3340d4ad6ff180c05
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2022-01-25 06:21:47 -0600

    Update sketch.js

--- Graphics/Graphics Assignment 1/Example 1/sketch.js
+++ Graphics/Graphics Assignment 1/Example 1/sketch.js
@@ -1,1 +1,19 @@
+function setup() {
+	createCanvas(200, 100);
+}
 
+function draw() {
+	var red = 119;
+	var green = 242;
+	var blue = 59;
+	
+    background(red, green, blue);
+	ellipse(50, 50, 80, 80); /* circle */
+	
+    var x = 110;
+	var y = 10;
+	var w = 80;
+	var h = 80;
+	
+    rect(x, y, w, h); /* square */
+}
