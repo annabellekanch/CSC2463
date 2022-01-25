1e4f0259dd0c5684048529e3785cf6e8788d18a6
Annabelle Kanchirathingal <77216805+annabellekanch@users.noreply.github.com>
2022-01-25 10:59:48 -0600

    Update sketch.js

--- Graphics/Graphics Assignment 1/Example 3/sketch.js
+++ Graphics/Graphics Assignment 1/Example 3/sketch.js
@@ -1,1 +1,31 @@
+/*
+Annabelle Kanchirathingal
+Drawing of Pac-Man & Ghost
+*/
 
+function setup() 
+{
+	angleMode(DEGREES);
+	createCanvas(300, 200); 
+	background("black");
+}
+
+function draw()
+{
+	noStroke();
+
+	fill("yellow");
+	arc(100, 100, 50, 50, 225, 135); 
+
+	fill("red");
+	rect(150, 100, 50, 25); 
+	ellipse(175, 100, 50, 50); 
+
+	fill("white");
+	ellipse(165, 95, 15, 15);
+	ellipse(185, 95, 15, 15);
+
+	fill("blue");
+	ellipse(165, 95, 9, 9);
+  	ellipse(185, 95, 9, 9);
+}
